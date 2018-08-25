e462ddd42137be3e978e437e32ae5fa0df2af31a
arvention <amc.antioquia@yahoo.com.ph>
2018-08-25 11:02:52 +0800

    indentation correction

--- solver.py
+++ solver.py
@@ -33,7 +33,7 @@ class Solver(object):
         """
 
         # instantiate Faster R-CNN model
-        self.model = FasterRCNN(self.input_channels, self.class_count)
+        # self.model = FasterRCNN(self.input_channels, self.class_count)
 
         # TODO: instantiate loss criterion
 
@@ -81,16 +81,15 @@ class Solver(object):
         elapsed = str(datetime.timedelta(seconds=elapsed))
 
         log = "Elapsed {}/{} -- {}, Epoch [{}/{}] Iter [{}/{}]," \
-              "loss: {:.4f}".format(
-               elapsed,
-               epoch_time,
-               total_time,
-               e + 1,
-               self.num_epochs,
-               i + 1,
-               iters_per_epoch,
-               loss
-               )
+              "loss: {:.4f}".format(elapsed,
+                                    epoch_time,
+                                    total_time,
+                                    e + 1,
+                                    self.num_epochs,
+                                    i + 1,
+                                    iters_per_epoch,
+                                    loss
+                                    )
 
         # TODO: add tensorboard
 
