1917f1780a24f76a269c387c02852144f6d917ee
arvention <amc.antioquia@yahoo.com.ph>
2018-08-28 23:37:13 +0800

    changed from config.dataset to config.voc_dataset

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,4 +1,4 @@
-from pascal_voc import get_voc_loader
+from data_loader.pascal_voc import get_voc_loader
 
 
 def get_loader(config):
@@ -12,11 +12,11 @@ def get_loader(config):
         if config.mode == 'train':
             data_path = config.voc_data_path + config.voc_train_data_path
             train_data_loader = get_voc_loader(data_path=data_path,
-                                               dataset=config.dataset,
+                                               dataset=config.voc_dataset,
                                                batch_size=config.batch_size,
                                                mode='train')
             test_data_loader = get_voc_loader(data_path=data_path,
-                                              dataset=config.dataset,
+                                              dataset=config.voc_dataset,
                                               batch_size=config.batch_size,
                                               mode='val')
 
